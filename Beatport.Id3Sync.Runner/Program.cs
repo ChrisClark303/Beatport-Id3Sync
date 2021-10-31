29cbf918aa720f2343368a04e2470bb949c2f870
Chris Clark <Chris.Clark@jet2.com>
2021-10-31 09:56:47 +0000

    Add project files.

---
+++ Beatport.Id3Sync.Runner/Program.cs
@@ -0,0 +1,16 @@
+﻿using Beatport.Id3Sync.TagManager;
+using System;
+
+namespace Beatport.Id3Sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new TagDataManager();
+            //manager.WriteTagData(args[0]);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Beatport.Id3Sync.TagManager/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Beatport.Id3Sync.TagManager
+{
+    public class TagDataManager
+    {
+        
+    }
+}
